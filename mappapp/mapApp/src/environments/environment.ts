14e721945af5cfb93beb4693b5623388d6aff4cf
ColvinBateman <46699025+ColvinBateman@users.noreply.github.com>
2019-04-29 19:10:38 -0600

    mapApp fix?

---
+++ mappapp/mapApp/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+
+    apiKey: "AIzaSyB5HmOxDx4sBAtd31QeL20Iujzkcvz6PQg",
+    authDomain: "mapapp-c0132.firebaseapp.com",
+    databaseURL: "https://mapapp-c0132.firebaseio.com",
+    projectId: "mapapp-c0132",
+    storageBucket: "mapapp-c0132.appspot.com",
+    messagingSenderId: "878204054059"
+
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
